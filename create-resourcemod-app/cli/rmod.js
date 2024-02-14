7ad37a37cedc5a3526ba85503bb4e40c7b2a94e3
Pedro Roque <pedro.roque@muzz.com>
2024-02-14 23:11:05 +0000

    initial cli update

--- create-resourcemod-app/cli/rmod.js
+++ create-resourcemod-app/cli/rmod.js
@@ -1,59 +1,101 @@
 #!/usr/bin/env node
 
-const https = require('node:https');
-const fs = require('node:fs');
-const {execSync} = require('node:child_process')
-const {platform} = require('node:process');
+const https = require("node:https");
+const fs = require("node:fs");
+const { execSync } = require("node:child_process");
+const { platform } = require("node:process");
+const readline = require("node:readline");
 
-if (platform !== 'win32') {
-    console.error('ResourceMod only runs on Windows.');
-    return false;
-}
-if (!fs.existsSync('./metamod')) {
-    console.error('MetaMod is not installed.');
-    console.error('Please install MetaMod first: https://wiki.alliedmods.net/Installing_metamod:source');
-    return false;
+if (platform !== "win32") {
+  console.error("ResourceMod only runs on Windows.");
+  return false;
 }
 
-const node_loader = fs.createWriteStream("./metamod/.node-loader.vdf");
-https.get("https://s3.eu-central-1.amazonaws.com/saido.static/vdfs/.node-loader.vdf", function (response) {
-    response.pipe(node_loader);
-    node_loader.on("finish", () => {
-        node_loader.close();
-        console.log("[NodeLoader] Download Completed!");
-    });
+if (!fs.existsSync("./metamod")) {
+  console.error("MetaMod is not installed.");
+  console.error(
+    "Please install MetaMod first: https://wiki.alliedmods.net/Installing_metamod:source"
+  );
+  return false;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const resourcemod = fs.createWriteStream("./metamod/resourcemod.vdf");
-https.get("https://s3.eu-central-1.amazonaws.com/saido.static/vdfs/resourcemod.vdf", function (response) {
-    response.pipe(resourcemod);
-    resourcemod.on("finish", () => {
-        resourcemod.close();
-        console.log("[ResourceMod] Download Completed!");
-    });
-});
+rl.question(
+  `Which language do you want to use? 
+    Enter 'javascript' or 'typescript'.
+    Press enter to use the default (javascript): `,
+  (answer) => {
+    let repository;
+    if (answer === "" || answer === "javascript") {
+      var tag = "v1.0.10";
+      repository = "https://github.com/resourcemod/package-template.git";
+    } else if (answer === "typescript") {
+      // var tag = 'v1.0.10';
+      repository = "https://github.com/resourcemod/package-template-typescript.git";
+    } else {
+      console.error(
+        "Invalid option. Please choose either javascript or typescript."
+      );
+      rl.close();
+      return false;
+    }
+    // rest of the code...
 
-try {
-    const repository = 'https://github.com/resourcemod/package-template.git'
-    const tag = 'v1.0.22'
-    execSync(`git clone --depth 1 --branch ${tag} ${repository} resourcemod`);
-} catch (e) {
-    console.error("Failed to clone resourcemod package.");
-    return false;
-}
+    const node_loader = fs.createWriteStream("./metamod/.node-loader.vdf");
+    https.get(
+      "https://s3.eu-central-1.amazonaws.com/saido.static/vdfs/.node-loader.vdf",
+      function (response) {
+        response.pipe(node_loader);
+        node_loader.on("finish", () => {
+          node_loader.close();
+          console.log("[NodeLoader] Download Completed!");
+        });
+      }
+    );
 
-try {
-    execSync(`cd resourcemod && npm install`);
-} catch (e) {
-    console.error("Failed to install resourcemod package.");
-    return false;
-}
+    const resourcemod = fs.createWriteStream("./metamod/resourcemod.vdf");
+    https.get(
+      "https://s3.eu-central-1.amazonaws.com/saido.static/vdfs/resourcemod.vdf",
+      function (response) {
+        response.pipe(resourcemod);
+        resourcemod.on("finish", () => {
+          resourcemod.close();
+          console.log("[ResourceMod] Download Completed!");
+        });
+      }
+    );
 
-try {
-    fs.rmdirSync("./resourcemod/.git", {
-        recursive: true
-    })
-} catch (e) {
-    console.error("Failed to remove .git dependencies.")
-    return false;
-}
+    try {
+      if (tag) {
+        execSync(`git clone --depth 1 --branch ${tag} ${repository} resourcemod`);
+      } else {
+        execSync(`git clone --depth 1 ${repository} resourcemod`);
+      }
+    } catch (e) {
+      console.error("Failed to clone resourcemod package.");
+      return false;
+    }
+
+    try {
+      execSync(`cd resourcemod && npm install`);
+    } catch (e) {
+      console.error("Failed to install resourcemod package.");
+      return false;
+    }
+
+    try {
+      fs.rmdirSync("./resourcemod/.git", {
+        recursive: true,
+      });
+    } catch (e) {
+      console.error("Failed to remove .git dependencies.");
+      return false;
+    }
+
+    rl.close();
+  }
+);
