2649de42f96259fa989c926e04119fbf3fda7732
Sergey Zyryanov <sergey.zyryanov@saido.pro>
2024-02-15 17:37:39 +0700

    fix: chat colors formatting

--- api/src/node/chat.ts
+++ api/src/node/chat.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import { metacall } from "metacall"
-import { GameMessageTarget } from "./constants"
+import {metacall} from "metacall"
+import {GameMessageTarget} from "./constants"
 
 export const sayToEveryone = (message: string, destination: GameMessageTarget = GameMessageTarget.Chat) => {
     if (message.match(/^\\x\d+/gm)) message = ` ${message}`
@@ -13,19 +13,19 @@ export const sayToSlot = (slot: number, message: string, destination: GameMessag
 }
 
 export enum Colors {
-    Default = "\x01",
-    DarkRed = "\x02",
-    LightPurple = "\x03",
-    Green = "\x04",
-    Olive = "\x05",
-    Lime = "\x06",
-    Red = "\x07",
-    Grey = "\x08",
-    Yellow = "\x09",
-    Silver = "\x0A",
-    LightBlue = "\x0B",
-    Blue = "\x0C",
-    Purple = "\x0E",
-    LightRed = "\x0F",
-    Orange = "\x10"
+    Default = " \x01",
+    DarkRed = " \x02",
+    LightPurple = " \x03",
+    Green = " \x04",
+    Olive = " \x05",
+    Lime = " \x06",
+    Red = " \x07",
+    Grey = " \x08",
+    Yellow = " \x09",
+    Silver = " \x0A",
+    LightBlue = " \x0B",
+    Blue = " \x0C",
+    Purple = " \x0E",
+    LightRed = " \x0F",
+    Orange = " \x10"
 }
