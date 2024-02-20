680ab1b31f518f846de3c91d9cf56842d3106d87
Sergey Zyryanov <sergey.zyryanov@saido.pro>
2024-02-21 02:53:31 +0700

    test: js api

--- api/src/game/index.ts
+++ api/src/game/index.ts
@@ -1,8 +1,11 @@
 import { Weapon, give, GearSlot, getWeaponFromGearSlot } from "./weapons"
+import {Vec3} from 'vec3'
+import * as entities from './entities'
 
 export default {
     Weapon,
     GearSlot,
     give,
-    getWeaponFromGearSlot
+    getWeaponFromGearSlot,
+    entities
 }
